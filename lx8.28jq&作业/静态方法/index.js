e02fc1774e8a25af2b1bcb219e4cf53764e10356
anruowang <1137211995@qq.com>
2016-08-31 19:51:42 +0800

    雪花&nodejs

--- lx8.28jq&作业/静态方法/index.js
+++ lx8.28jq&作业/静态方法/index.js
@@ -4,7 +4,7 @@
 // @params newOn:新图标出现的频率
 // @params flakeColor:颜色
 
-;(function(w,d,u,$){
+;function(w,d,u,$){
 	$.fn.showPic=function(options){
 		var defaults={
 			flakeChar:"&#10047",
@@ -20,7 +20,7 @@
 		//设置出现的位置
 		//页面多大
 		var documentHeight=$(d).height();
-		var documentWidth=$(d).width();
+		var documentWidth=$(id).width();
 
 		$flake.html(defaults.flakeChar);
 
@@ -29,32 +29,12 @@
 		setInterval(function(){
 		//计算起始位置
 			var startPositionLeft=Math.random()*documentWidth-100;//div的x坐标
-			var startOpacity=Math.random()+0.5;//透明度
-			var sizeFlake=defaults.minsize+Math.random()*defaults.maxSize;//雪花大小
-			var endPositionLeft=startPositionLeft-100+Math.random()*200;
-			var endPositionTop=documentHeight-defaults.maxSize;
-			var durationFall=documentHeight*5+Math.random()*5000;
+			var startOpacity
 		//大小
 		//再clone $flake
-		$flake.clone()
-			.appendTo("body")
-			.css({
-				left:startPositionLeft,
-				opacity:startOpacity,
-				"font-size":sizeFlake,
-				color:defaults.flakeColor
-			})
 		//设置clone出来的 flake的css值
 		//通过animate设置flake的动画（坐标，透明度，大小）
-		.animate({
-			top:endPositionTop,
-			left:endPositionLeft,
-			opacity:0.1
-
-		},durationFall,'linear',function(){
-			$(this).remove();
-		})
 		}, 100);
 		
 	}
-})(window,document,undefined,$);
+}(window,document,undefined,$);
