3bea248ff46b93c6013eeec404312e78d319b61a
anruowang <1137211995@qq.com>
2016-09-04 11:56:52 +0800

    move node

--- lx8.31node/yc2/node2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2016/8/31.
- */
-var buf=new Buffer([0x10,0x20]);
-console.info(buf.readUInt8(0));
-console.info(buf.readUInt8(1));
-
-buf.writeUInt8(30,0);
-console.info(buf);
